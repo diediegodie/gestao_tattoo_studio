15460ae15efaf4f98be5c3a28903dac61a6c4a7b
Diego Rodriguez <diegotattoosp@gmail.com>
2025-07-10 17:27:09 -0300

    update

--- test_import_historico.py
+++
@@ -1,7 +0,0 @@
-try:
-    import historico.routes
-    print("Importação de historico.routes OK!")
-except Exception as e:
-    print("ERRO ao importar historico.routes:")
-    import traceback
-    traceback.print_exc() 
